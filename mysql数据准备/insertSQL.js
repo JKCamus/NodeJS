dff60254057274d2c0e388d427906e7a8b961afb
JKCamus <924850758@qq.com>
2020-11-19 10:28:46 +0800

    运行，并做测试

--- mysql数据准备/insertSQL.js
+++ mysql数据准备/insertSQL.js
@@ -1,17 +1,31 @@
-const mysql = require('mysql2');
- 
+/**
+ * @description: 首先创建表格
+ * 
+CREATE TABLE IF NOT EXISTS `products`(
+id INT PRIMARY KEY auto_increment,
+brand VARCHAR(20),
+title VARCHAR(100) NOT NULL,
+price DOUBLE NOT NULL,
+score DECIMAL(2,1),
+voteCnt INT,
+url VARCHAR(100),
+pid INT
+);
+ */
+
+const mysql = require("mysql2");
+
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: "localhost",
   port: 3306,
-  user: 'root',
-  password: 'Coderwhy888.',
-  database: 'coderhub'
+  user: "root",
+  password: "qwertyuiop",
+  database: "camushub",
 });
 
-const statement = `INSERT INTO products SET ?;`
-const phoneJson = require('./phone.json');
+const statement = `INSERT INTO products SET ?;`;
+const phoneJson = require("./phone.json");
 
 for (let phone of phoneJson) {
   connection.query(statement, phone);
 }
-
