29f7725c90c5ccfbddb2e97e2f30db5eebcbc6b0
JKCamus <924850758@qq.com>
2020-11-21 12:23:27 +0800

    sequelize单表查询

--- mysql数据准备/sequelize单表.js
+++ mysql数据准备/sequelize单表.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes, Model,Op } = require("sequelize");
 const sequelize = new Sequelize("camushub", "root", "qwertyuiop", {
   host: "localhost",
   dialect: "mysql",
@@ -15,3 +15,44 @@ sequelize
   .catch((err) => {
     console.log("连接数据库失败~");
   });
+class Product extends Model {}
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: DataTypes.DOUBLE,
+    score: DataTypes.DOUBLE,
+  },
+  {
+    table: "products",
+    /* 默认有底下两个字段，但是这张表没有 */
+    createdAt: false,
+    updatedAt: false,
+    sequelize,
+  }
+);
+
+async function queryProduct(params) {
+  // 1.查询数据库中product表中所有的内容
+  const result = await Product.findAll({
+    // 有条件查询的时候需要添加大括号，然后添加where，
+    // 引入Op依赖，gte为大于等于，gt为大于，lt为小于
+    where: {
+      price: {
+        [Op.gte]: 5000,
+      },
+    },
+  });
+  console.log("res", result);
+  // const result=await Product.update({
+
+  // })
+}
+queryProduct()
