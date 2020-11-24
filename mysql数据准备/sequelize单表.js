07c759b71b27a2acc106686fa20f342dbca332d6
JKCamus <924850758@qq.com>
2020-11-24 16:03:27 +0800

    sequelize一对多

--- mysql数据准备/sequelize单表.js
+++ mysql数据准备/sequelize单表.js
@@ -41,18 +41,32 @@ Product.init(
 
 async function queryProduct(params) {
   // 1.查询数据库中product表中所有的内容
-  const result = await Product.findAll({
-    // 有条件查询的时候需要添加大括号，然后添加where，
-    // 引入Op依赖，gte为大于等于，gt为大于，lt为小于
-    where: {
-      price: {
-        [Op.gte]: 5000,
-      },
-    },
-  });
-  console.log("res", result);
-  // const result=await Product.update({
+  // const result = await Product.findAll({
+  //   // 有条件查询的时候需要添加大括号，然后添加where，
+  //   // 引入Op依赖，gte为大于等于，gt为大于，lt为小于
+  //   where: {
+  //     price: {
+  //       [Op.gte]: 5000,
+  //     },
+  //   },
+  // });
+  // console.log("res", result);
+   // 2.插入数据
+  // const result = await Product.create({
+  //   title: "三星Nova",
+  //   price: 8888,
+  //   score: 5.5
+  // });
+  // console.log(result);
 
-  // })
+  // 3.更新数据
+  const result = await Product.update({
+    price: 3688
+  }, {
+    where: {
+      id: 1
+    }
+  });
+  console.log(result);
 }
 queryProduct()
