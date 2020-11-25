462463f9c52656f1f3ebeeaa8ca4dcf6167b7493
JKCamus <924850758@qq.com>
2020-11-25 09:38:39 +0800

    new: 用户简单注册成功，数据库连接成功，并且插入数据

--- camushub/src/service/user.service.js
+++ camushub/src/service/user.service.js
@@ -1,23 +1,23 @@
 /*
- * @Description: 用于处理查询数据库相关逻辑
+ * @Description: 用于处理查询数据库相关逻辑，服务于controller
  * @version:
  * @Author: camus
  * @Date: 2020-11-24 21:24:17
  * @LastEditors: camus
- * @LastEditTime: 2020-11-24 21:58:13
+ * @LastEditTime: 2020-11-25 09:37:13
  */
 
 const connection = require("../app/database");
 
 class UserService {
   async create(user) {
-      console.log('chaung', user)
-    // const { name, password } = user;
-    // /* mysql创建语句，创建对应的名字和密码，？为按顺序填入 */
-    // const statement = `INSERT INTO users (name, password) VALUES (?, ?);`;
-    // /* 执行相关的mysql语句，name，password按顺序填入 */
-    // const result = await connection.execute(statement, [name, password]);
-    // return result[0];
+    // 获取在user.controller中解析后的user
+    const { name, password } = user;
+    /* mysql创建语句，创建对应的名字和密码，？为按顺序填入 */
+    const statement = `INSERT INTO user (name, password) VALUES (?, ?);`;
+    /* 执行相关的mysql语句，数组中，name，password按顺序填入 */
+    const result = await connection.execute(statement, [name, password]);
+    return result[0];
   }
 }
 module.exports = new UserService();
