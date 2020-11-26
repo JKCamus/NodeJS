49a198687b1e044a97d917e5ef1704bcc580ba73
JKCamus <924850758@qq.com>
2020-11-26 16:04:00 +0800

    动态设置路由

--- camushub/src/app/index.js
+++ camushub/src/app/index.js
@@ -4,20 +4,21 @@
  * @Author: camus
  * @Date: 2020-11-24 17:22:23
  * @LastEditors: camus
- * @LastEditTime: 2020-11-26 11:36:34
+ * @LastEditTime: 2020-11-26 15:58:39
  */
 const Koa = require("koa");
 const app = new Koa();
-const userRouter = require("../router/user.router");
-const authRouter = require("../router/auth.router");
 const bodyParser = require("koa-bodyparser");
 const errorHandler = require("./error.handle");
+const useRoutes = require("../router");
 /* 挨个执行中间件 */
 app.use(bodyParser());
-app.use(userRouter.routes());
-/* 检测路由方法是否正确 */
-app.use(authRouter.routes());
-app.use(userRouter.allowedMethods());
+// app.use(userRouter.routes());
+// /* 检测路由方法是否正确 */
+// app.use(authRouter.routes());
+// 导入路由后，就可以不用再分开使用路由了
+useRoutes(app);
+// app.use(userRouter.allowedMethods());
 /* 监听事件error，当发生error事件的时候，会执行errorHandler */
 app.on("error", errorHandler);
 
