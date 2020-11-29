ff359f3d747610903814503ddb2808827d8375f2
JKCamus <924850758@qq.com>
2020-11-29 15:34:55 +0800

    刪除回復，回复的回复会一并被删除

--- camushub/src/router/comment.router.js
+++ camushub/src/router/comment.router.js
@@ -4,15 +4,18 @@
  * @Author: camus
  * @Date: 2020-11-29 14:04:33
  * @LastEditors: camus
- * @LastEditTime: 2020-11-29 14:12:10
+ * @LastEditTime: 2020-11-29 15:28:51
  */
 const Router = require("koa-router");
 const commentRouter = new Router({ prefix: "/comment" });
-const { create } = require("../controller/comment.controller");
+const { create,reply,update,remove} = require("../controller/comment.controller");
 
 const { verifyAuth } = require("../middleware/auth.middleware");
 const { verifyPermission } = require("../middleware/auth.middleware");
 
 commentRouter.post("/", verifyAuth, create);
+commentRouter.post("/reply/:commentId", verifyAuth, reply);
+commentRouter.patch('/:commentId',verifyAuth,verifyPermission,update)
+commentRouter.delete('/:commentId',verifyAuth,verifyPermission,remove)
 
 module.exports = commentRouter
--- camushub/src/service/comment.service.js
+++ camushub/src/service/comment.service.js
@@ -4,7 +4,7 @@
  * @Author: camus
  * @Date: 2020-11-29 14:15:01
  * @LastEditors: camus
- * @LastEditTime: 2020-11-29 14:24:08
+ * @LastEditTime: 2020-11-29 15:33:25
  */
 const connection = require("../app/database");
 
@@ -20,6 +20,43 @@ class CommentService {
       return result;
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  async reply(momentId, content, userId, commentId) {
+    try {
+      const statement = `INSERT INTO comment (content, moment_id, user_id, comment_id) VALUES (?, ?, ?, ?);`;
+      const [result] = await connection.execute(statement, [
+        content,
+        momentId,
+        userId,
+        commentId,
+      ]);
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async update(content, commentId) {
+    try {
+      const statement = `UPDATE comment SET content = ? WHERE id = ?`;
+      const [result] = await connection.execute(statement, [
+        content,
+        commentId,
+      ]);
+      return result;
+    } catch (error) {
+      console.log("CommentService.update", error);
+    }
+  }
+
+  async remove(commentId) {
+    try {
+      const statement = `DELETE FROM comment WHERE id = ?`;
+      const [result] = await connection.execute(statement, [commentId]);
+      return result;
+    } catch (error) {
+      console.log("CommentService.remove", error);
     }
   }
 }
